fbfb163461964401f4dc251989a6b5c29c8c51a9
mywalter <48268044+mywalter@users.noreply.github.com>
2019-03-06 10:53:28 +0800

    Add files via upload

---
+++ registration.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: walter
+ * Date: 2018-12-13
+ * Email: mywalter1985@gmail.com
+ * Time: 22:17
+ */
+
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+        \Magento\Framework\Component\ComponentRegistrar::MODULE,
+        'BuyQuickly_Slider',
+        __DIR__
+    );
